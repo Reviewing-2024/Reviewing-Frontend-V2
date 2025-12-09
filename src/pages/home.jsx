4507758225fd9bd9de17edcf55e7c4cb314ec096
unknown <wnsgus880@naver.com>
2025-12-09 16:51:33 +0900

    init develop 브랜치 생성

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -7,6 +7,19 @@ const home = () => {
     <div id='home' role='home'>
       <div className='home__banner'>
         <img className='banner' src="/img/banner.png" alt="banner" />
+          <input 
+              type='search' 
+              id='searchInput' 
+              placeholder='검색어를 입력해주세요.' 
+              autoComplete='off' 
+              className='searchinput' 
+              onChange={e => setSearchKeyword(e.target.value)}
+              onKeyDown={e => {
+                  if (e.key === 'Enter') {
+                    handleSearch();
+                  }
+              }}
+            />
       </div>
     </div>
   )
