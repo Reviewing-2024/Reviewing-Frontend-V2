398ac633f62bb847581a277ac31e59ad6e964ea7
sungjunhyeon <wnsgus880@naver.com>
2025-12-19 11:21:16 +0900

    feat detail 강의 상세 컴포넌트 틀잡기

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,12 +1,70 @@
 import React from 'react'
-import { Outlet, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
+
+import '../assets/scss/section/_detail.scss'
+
+import { course } from '../data/course'
 
 const Detail = () => {
-  const params = useParams();
+
+    const slug = useParams();
+
+    console.log(slug.slug)
+
+    let courses = course.find(function(x){
+        return x.slug == slug.slug
+      });
 
 
   return (
-    <div><p>{params.slug}</p></div>
+    <section id='detail'>
+      <div className='detail__container'>
+        <div className='detail-img-container'>
+          <img src={courses.thumbnailImage} alt="이미지" />
+        </div>
+        <div className='detail-information'>
+          <div className='detail-information-container'>
+            <span className='information-platform'>inflearn</span>
+            <h1>{courses.title}</h1>
+            <p>{courses.teacher}</p>
+            <span className='information-rating'>{courses.rating}</span>
+          </div>
+          <div className='information-container-btn'>
+            <button className='detail-btn'>강의 페이지로 이동</button>
+            <button className='detail-wish-btn'>하트</button>
+          </div>
+        </div>
+      </div>
+      <div className='review-container'>
+        <div className='review-header'>
+          
+            <h2>수강생 리뷰</h2>
+          
+          
+          <div className='review-category'>
+            <ul>
+              <li>
+                <button>최신순</button>
+              </li>
+              <li>
+                <button>높은 평점순</button>
+              </li>
+              <li>
+                <button>낮은 평점순</button>
+              </li>
+            </ul>
+            <div className='review-write'>
+                <button>리뷰 작성하기</button>
+            </div>
+          </div>
+        </div>
+        <div className='review-list'>
+          <div className='review-caerd'>
+
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
