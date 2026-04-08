c022b22bc30c3caa67cbeb19b3e80325dc3e2ace
sungjunhyeon <wnsgus880@naver.com>
2026-04-08 14:39:28 +0900

    feat wish페이지 페이지네이션 추가, wishlistCount ui 색상 변경

--- src/pages/Wish.jsx
+++ src/pages/Wish.jsx
@@ -1,28 +1,32 @@
 import React, { useState, useEffect } from 'react'
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams, useSearchParams } from 'react-router-dom';
 import axios from 'axios';
+
+import CourseCard from '../components/component/CourseCard';
+import Pagination from '../components/component/Pagination';
+
+import '../asserts/scss/section/_wish.scss'
 
 import { mypage_Sort_Category, mypage_Review_Category } from '../data/mypagedata'
 import { PiDotsThreeOutlineFill } from "react-icons/pi";
 import { FaHeart, FaRegHeart, FaThumbsDown, FaThumbsUp } from "react-icons/fa6";
 
-import '../asserts/scss/section/_wish.scss'
-
 import { useAuth } from "../context/AuthContext";
 import { handleApiError } from '../data/apierror.js'
 
-import CourseCard from '../components/component/CourseCard';
-import Pagination from '../components/component/Pagination';
 
 const Wish = () => {
-
+  const [searchParams, setSearchParams] = useSearchParams();
   const { sortCategory } = useParams();
   const [courses, setCourses] = useState([]);
-  const [courseCount, setCourseCount] = useState([]);
+  const [courseCount, setCourseCount] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
   const [current_category, setCurrent_category] = useState('wish');
 
   const { logout } = useAuth();
   const accessToken = localStorage.getItem("accessToken");
+  const page = Number(searchParams.get('page')) || 1;
+  const ITEMS_PER_PAGE = 12;
 
   useEffect(() => {
     if (sortCategory) {
@@ -38,15 +42,16 @@ const Wish = () => {
     try {
 
       const response = await axios.get(
-        `${import.meta.env.VITE_API_BASE_URL}/api/v1/members/me/wishes`, 
-        {headers: { Authorization: accessToken }}
+        `${import.meta.env.VITE_API_BASE_URL}/api/v1/members/me/wishes`,
+        { headers: { Authorization: accessToken } }
       );
 
       setCourses(response.data.data.content);
       setCourseCount(response.data.data.page.totalElements);
+      setTotalPages(response.data.data.page.totalPages);
 
     } catch (error) {
-      handleApiError(error,{logout});
+      handleApiError(error, { logout });
     }
 
   };
@@ -76,20 +81,31 @@ const Wish = () => {
         </ul>
       </nav>
       <div>
-          <div className='wishlist_count'>
-            <FaHeart /> <span>찜한 강의</span><span>{courseCount}개</span>
-          </div>
-      
-      <div className='home__item'>
-        <div className='item__card'>
-          {courses.map(course => (
-            <CourseCard  course={course} key={course.id}/>
-          ))}
+        <div className='wishlist_count'>
+          <FaHeart /> <span>찜한 강의</span><span className='courseCount'>{courseCount}개</span>
         </div>
+
+        <div className='home__item'>
+          <div className='item__card'>
+            {courses.map(course => (
+              <CourseCard course={course} key={course.id} />
+            ))}
+          </div>
+        </div>
+
       </div>
-      
-      </div>
-      </div>
+      <Pagination
+        currentPage={page}
+        totalPages={totalPages}
+        onPageChange={(p) => {
+          const newParams = new URLSearchParams(searchParams);
+
+          newParams.set("page", p);
+
+          setSearchParams(newParams);
+        }}
+      />
+    </div>
 
   )
 }
